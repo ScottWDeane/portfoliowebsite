79d56ae3abae39b6ff4c3b9eba0972b45fc9cade
ScottWDeane609 <ScottWDeane@gmail.com>
2018-08-05 23:39:42 +0000

    Initial commit.

---
+++ app.js
@@ -0,0 +1,23 @@
+/*  Purpose: To be my own personal website.
+    Have sections and links about my portfolio and any projects I'm currently working on.
+    Will include my Python Job Application tracker, with screenshots, and new projects along the way.
+    Start off small, with limited functionality, but will eventually add in new features as I fix my bloody IDE.
+    Eventually would love to include mongoose/mongodb for adding/deleting content to show-off dynamic web pages.
+    RESTful routing.
+    
+*/
+
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser");
+    
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("My personal website server has started..."); 
+});
