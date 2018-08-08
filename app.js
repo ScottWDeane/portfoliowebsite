106006254246867e555f1e76a341c0e66162a4d1
ScottWDeane609 <ScottWDeane@gmail.com>
2018-08-08 02:50:01 +0000

    Re-organized routes, added in connection to a (for now) blank master CSS file, added in bootstrap4 Jumbotron into projects page.

--- app.js
+++ app.js
@@ -13,10 +13,15 @@ var express = require("express"),
     
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
     res.render("home");
 });
+
+app.get("/projects", function(req, res){
+    res.render("projects");
+})
 
 app.get("/socialMedia", function(req, res){
     res.render("socialMedia");
